24ee3d9e9329232a9dd7e09a40e66b1bdfce277f
PHAEDRIA <phaedriathayer@gmail.com>
2023-01-30 12:11:47 -0800

    updates

--- Ireland/JS/main.js
+++ Ireland/JS/main.js
@@ -9,7 +9,7 @@ function openModal() {
 //close modal
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
-}
+};
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -29,11 +29,13 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
+    
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
